a69b9297b96be7b3f53fc8f1cf139e706e181a3d
Dan Leonard <dcl525@gmail.com>
2022-12-21 05:17:09 +0000

    Merged PR 1068: Remove FeatureClient Import
    Remove FeatureClient import

--- framework/clients/__init__.py
+++ framework/clients/__init__.py
@@ -1,5 +1,5 @@
 from framework.clients.cache_client import CacheClient, CacheClientAsync
-from framework.clients.feature_client import FeatureClient, FeatureClientAsync
+from framework.clients.feature_client import FeatureClientAsync
 from framework.clients.http_client import HttpClient
 # from framework.clients.identity_client import IdentityClient
 # from framework.clients.keyvault_client import AzureKeyVaultClient
