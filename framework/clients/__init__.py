9637d9a426ff79ef1c411c5052907f480cf759ee
Dan Leonard <dcl525@gmail.com>
2022-12-21 09:25:12 +0000

    Merged PR 1069: v0.0.8
    Bump version

--- framework/clients/__init__.py
+++ framework/clients/__init__.py
@@ -1,5 +1,3 @@
 from framework.clients.cache_client import CacheClient, CacheClientAsync
 from framework.clients.feature_client import FeatureClientAsync
 from framework.clients.http_client import HttpClient
-# from framework.clients.identity_client import IdentityClient
-# from framework.clients.keyvault_client import AzureKeyVaultClient
