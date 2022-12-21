0e883f6f5fa4cc1cc56852fb30f8893970da0818
Dan Leonard <dcl525@gmail.com>
2022-12-21 03:55:26 +0000

    Merged PR 1065: v0.0.7
    - Changes to feature client
    - Add default header key

--- framework/clients/feature_client.py
+++ framework/clients/feature_client.py
@@ -1,34 +1,44 @@
 from typing import Any, Tuple
 
-import requests
-from framework.clients.http_client import HttpClient
-from framework.configuration.configuration import Configuration
-from framework.logger.providers import get_logger
 import httpx
+
+from framework.configuration.configuration import Configuration
+from framework.exceptions.nulls import ArgumentNullException
+from framework.logger.providers import get_logger
 
 logger = get_logger(__name__)
 
-DEFAULT_HEADER_KEY = 'api-key'
+DEFAULT_HEADER_KEY = 'X-Api-Key'
 
 
-class FeatureClient:
+class FeatureClientAsync:
     def __init__(
         self,
         configuration: Configuration
     ):
-        self.__enabled = (
-            configuration.features.get('enabled') or True
-        )
+        self.__base_url = configuration.features.get('base_url')
+        self.__api_key = configuration.features.get('api_key')
 
-        if self.__enabled:
-            self.base_url = configuration.features.get('base_url')
-            self.api_key = configuration.features.get('api_key')
+        self.__header_key = configuration.features.get(
+            'header_key', DEFAULT_HEADER_KEY)
+
+        self.__validate_config()
+
+    def __validate_config(
+        self
+    ):
+        ArgumentNullException.if_none_or_whitespace(
+            self.__base_url, 'base_url')
+        ArgumentNullException.if_none_or_whitespace(
+            self.__api_key, 'api_key')
+        ArgumentNullException.if_none_or_whitespace(
+            self.__header_key, 'header_key')
 
     def __get_headers(
         self
     ) -> dict:
         return {
-            'api-key': self.api_key
+            self.__header_key: self.__api_key
         }
 
     def get_disabled_feature_response(
@@ -46,7 +56,7 @@ class FeatureClient:
             'message': f"Feature '{feature_key}' is not enabled"
         }, 200
 
-    def is_enabled(
+    async def is_enabled(
         self,
         feature_key: str
     ) -> Any:
@@ -56,74 +66,27 @@ class FeatureClient:
         `feature_key`: feature flag key
         '''
 
+        logger.info(f'Evaluating feature flag: {feature_key}')
+
         if not self.__enabled:
             logger.info(f'Feature evaluation is disabled')
             return True
 
-        logger.info(f'Evaluating feature flag: {feature_key}')
+        endpoint = f'{self.__base_url}/api/feature/evaluate/{feature_key}'
+        headers = self.__get_headers()
+
+        logger.info(f'Endpoint: {endpoint}')
+        logger.info(f'Headers: {headers}')
+
         try:
-            response = requests.get(
-                url=f'{self.base_url}/api/feature/evaluate/{feature_key}',
-                headers=self.__get_headers())
+            async with httpx.AsyncClient(timeout=None) as client:
+                response = await client.get(
+                    url=endpoint,
+                    headers=headers)
 
             content = response.json()
             return content.get('value')
         except Exception as ex:
             logger.info(
                 f'Failed to fetch feature flag: {feature_key}: {str(ex)}')
-            return False
-
-
-class FeatureClientAsync:
-    def __init__(
-        self,
-        configuration: Configuration
-    ):
-        self.enabled = configuration.features.get(
-            'enabled', True)
-
-        self.__base_url = configuration.features.get('base_url')
-        self.__api_key = configuration.features.get('api_key')
-
-        self.__header_key = configuration.features.get(
-            'header_key',
-            DEFAULT_HEADER_KEY)
-
-        self.http_client = HttpClient()
-
-    def get_headers(
-        self
-    ) -> dict:
-        return {
-            self.__header_key: self.__api_key
-        }
-
-    def get_disabled_feature_response(
-        self,
-        feature_key: str
-    ) -> Tuple[dict, int]:
-        return {
-            'message': f"Feature '{feature_key}' is not enabled"
-        }, 200
-
-    async def is_enabled(
-        self,
-        feature_key: str
-    ) -> Any:
-
-        if not self.enabled:
-            logger.info(f'Feature evaluation is disabled')
-            return True
-
-        logger.info(f'Evaluating feature flag: {feature_key}')
-        try:
-            async with httpx.AsyncClient(timeout=None) as client:
-                response = await client.get(
-                    url=f'{self.__base_url}/api/feature/evaluate/{feature_key}',
-                    headers=self.get_headers())
-
-            content = response.json()
-            return content.get('value')
-        except Exception as ex:
-            logger.exception(f'Failed to fetch feature flag: {feature_key}')
             return False
